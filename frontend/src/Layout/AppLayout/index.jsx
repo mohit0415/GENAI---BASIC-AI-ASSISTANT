b018d90fbbcc88463cf6c7920cffc7430ee7584c
mohit <madireddymohit@gmail.com>
2026-04-18 09:12:30 +0530

    Component Name Case Sensitive failed

--- frontend/src/Layout/AppLayout/index.jsx
+++ frontend/src/Layout/AppLayout/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import AppSidebar from "../AppSideBar";
 import AppHeader from "../AppHeader";
 import { Outlet } from "react-router-dom";
 
@@ -8,7 +7,6 @@ const AppLayout = () => {
   return (
     <div>
       <div className="flex flex-1">
-        {/* <AppSidebar isSideBarOpen={isSideBarOpen}/> */}
         <div className="flex flex-col  mt-0 w-full">
           <AppHeader setIsSideBarOpen={setIsSideBarOpen} isSideBarOpen={isSideBarOpen}/>
           <div className=" overflow-y-auto">
